30a084175e9d975eda8f537603bee51fac48181a
philgcc200011@fpt.edu.vn <98210418+LamPhiGCC200011@users.noreply.github.com>
2023-03-12 23:11:01 +0700

    Add Pages, information of members, fixing interface

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,14 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
 
     public IActionResult Privacy()
     {
