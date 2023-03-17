f9f13e986f707008f5ea8a405f3a15f389dc413f
Hunter <98210343+Hunter1725@users.noreply.github.com>
2023-03-17 18:09:09 +0700

    Fix avatar null

--- Migrations/20230317110828_InitialCreate.Designer.cs
+++ Migrations/20230317110828_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EnterpriseWeb.Migrations
 {
     [DbContext(typeof(EnterpriseWebIdentityDbContext))]
-    [Migration("20230317105545_InitialCreate")]
+    [Migration("20230317110828_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
