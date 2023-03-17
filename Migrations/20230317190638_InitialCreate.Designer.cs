49733fad5ea34904a275a46ccb8cfa36d805f617
philgcc200011@fpt.edu.vn <98210418+LamPhiGCC200011@users.noreply.github.com>
2023-03-18 03:00:56 +0700

    Change the interface of Create Idea

--- Migrations/20230317190638_InitialCreate.Designer.cs
+++ Migrations/20230317190638_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EnterpriseWeb.Migrations
 {
     [DbContext(typeof(EnterpriseWebIdentityDbContext))]
-    [Migration("20230317112808_InitialCreate")]
+    [Migration("20230317190638_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
