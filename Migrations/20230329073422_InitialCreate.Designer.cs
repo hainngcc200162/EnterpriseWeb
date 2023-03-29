3cd42ed5b9156b7b2bb525a70051ce7d4aff6537
ndtphuc <98210570+ndtphuc@users.noreply.github.com>
2023-03-29 14:39:28 +0700

    update index of User Roles page

--- Migrations/20230329073422_InitialCreate.Designer.cs
+++ Migrations/20230329073422_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EnterpriseWeb.Migrations
 {
     [DbContext(typeof(EnterpriseWebIdentityDbContext))]
-    [Migration("20230327150043_InitialCreate")]
+    [Migration("20230329073422_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
