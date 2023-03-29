99d1da9bb21b1469deb292886f4d67998494e564
Nguyen Ngoc Hai <hainngcc200162@fpt.edu.vn>
2023-03-29 18:40:47 +0700

    Catch exception (Unique Category and Department)

--- Areas/Identity/Data/EnterpriseWebIdentityDbContext.cs
+++ Areas/Identity/Data/EnterpriseWebIdentityDbContext.cs
@@ -16,6 +16,11 @@ public class EnterpriseWebIdentityDbContext : IdentityDbContext<IdeaUser>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        builder.Entity<EnterpriseWeb.Models.Department>()
+             .HasIndex(p => p.Name).IsUnique();
+        builder.Entity<EnterpriseWeb.Models.Category>()
+             .HasIndex(p => p.Name).IsUnique();
+        
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
